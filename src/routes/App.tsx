da9e02831ea6168d78c3e326ecb27b7b582cd67b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-05-11 00:35:00 +0700

    initialize project

---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="text-blue-700 underline">
+      <p> sdf app</p>
+    </div>
+  );
+}
+
+export default App;
